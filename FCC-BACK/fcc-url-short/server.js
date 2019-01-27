e177d0c522a2ebe9596b28a714a3a5e7fc3aac59
Eduardo Daniel Martínez Cesta <daniell9404@outlook.com>
2019-01-26 19:32:29 -0600

    2

--- FCC-BACK/fcc-url-short/server.js
+++ FCC-BACK/fcc-url-short/server.js
@@ -1,18 +1,21 @@
 'use strict';
 
+var fs = require('fs');
 var express = require('express');
 var mongo = require('mongodb');
 var mongoose = require('mongoose');
-
+var shortid = require('shortid');
+shortid.characters('0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ$&');
+var validUrl = require('valid-url');
 var cors = require('cors');
 
 var app = express();
-
+var MongoClient = mongo.MongoClient();
 // Basic Configuration 
 var port = process.env.PORT || 3000;
 
 /** this project needs a db !! **/ 
-// mongoose.connect(process.env.MONGOLAB_URI);
+//mongoose.connect(process.env.MONGO_URI);
 
 app.use(cors());
 
@@ -21,15 +24,111 @@ app.use(cors());
 
 app.use('/public', express.static(process.cwd() + '/public'));
 
-app.get('/', function(req, res){
+/*app.get('/', function(req, res){
   res.sendFile(process.cwd() + '/views/index.html');
+});*/
+
+app.route('/_api/package.json')
+  .get(function(req, res, next) {
+    console.log('requested');
+    fs.readFile(__dirname + '/package.json', function(err, data) {
+      if(err) return next(err);
+      res.type('txt').send(data.toString());
+    });
+  });
+  
+app.route('/')
+    .get(function(req, res) {
+		  res.sendFile(process.cwd() + '/views/index.html');
+    });
+
+app.route('/new/:url(*)')
+    .get( (req,res, next) => {
+  //connect to database
+  MongoClient.connect(process.env.MONGO_URL, (err, db) => {
+        if (err) {
+          console.log("Unable to connect to server", err);
+        } else {
+          //console.log("Connected to server");
+          let collection = db.collection('links');
+          let url = req.params.url;
+          let host = req.get('host') + "/"
+          
+          //function to generate short link 
+          let generateLink = function(db, callback) {
+            //check if url is valid
+            if (validUrl.isUri(url)){
+              collection.findOne({"url": url}, {"short": 1, "_id": 0}, (err, doc) =>{
+                if(doc != null){
+                  res.json({
+                  "original_url": url, 
+                  "short_url":host + doc.short
+                });
+                }
+                else{
+                   //generate a short code
+                    let shortCode = shortid.generate();
+                    let newUrl = { url: url, short: shortCode };
+                    collection.insert([newUrl]);
+                      res.json({
+                        "original_url":url, 
+                        "short_url":host + shortCode
+                      });
+                }
+              });
+            } 
+            else {
+                console.log('Not a URI');
+                res.json({
+                  "error": "Invalid url"
+                })
+            }
+          };
+          
+          generateLink(db, function(){
+            db.close();
+          });
+        }
+  }); 
 });
 
-  
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+//given short url redirect to original url
+app.route('/:short')
+    .get( (req,res, next) => {
+  MongoClient.connect(process.env.MONGO_URI, (err,db) => {
+    if (err) {
+          console.log("Unable to connect to server", err);
+        } else {
+          let collection = db.collection('links');
+          let short = req.params.short;
+          
+          //search for original url in db and redirect the browser
+          collection.findOne({"short": short}, {"url": 1, "_id": 0}, (err, doc) => {
+            if (doc != null) {
+              res.redirect(doc.url);
+            } else {
+              res.json({ error: "Shortlink not found in the database." });
+            };
+          });
+        }
+    db.close();
+  });
 });
+
+// Respond not found to all the wrong routes
+app.use(function(req, res, next){
+  res.status(404);
+  res.type('txt').send('Not found');
+});
+
+// Error Middleware
+app.use(function(err, req, res, next) {
+  if(err) {
+    res.status(err.status || 500)
+      .type('txt')
+      .send(err.message || 'SERVER ERROR');
+  }  
+})
 
 
 app.listen(port, function () {
